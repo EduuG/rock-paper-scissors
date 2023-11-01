362f2548197719137a55f0e06c8ba172bee9d363
Eduardo Guimarães <54005791+EduuG@users.noreply.github.com>
2023-11-01 01:40:23 -0300

    Code redesign completed

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function getComputerChoice() {
   }
 }
 
+
 function playRound(playerSelection, computerSelection) {
   const possibilities = ["rock", "paper", "scissors"];
   player = playerSelection.toLowerCase();
@@ -33,40 +34,45 @@ function playRound(playerSelection, computerSelection) {
     if (player === computer.defeats) return `You lose! ${computer.name} beats ${player}`;
     return `You win! ${player} beats ${computer.name}`;
   }
-
   return false
 }
+
 
 function game() {
   let scorePlayer = 0;
   let scoreComputer = 0;
 
-
   for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Rock, paper or scissors?");
+    while (true) {
+      const playerSelection = prompt("Rock, paper or scissors?");
+      const round = playRound(playerSelection, getComputerChoice());
 
-    let game = playRound(playerSelection, getComputerChoice());
+      if (!round) continue;
 
-    switch (game) {
-      case false: continue;
-      case game.includes("Tied!"): {
+      if (round.includes("Tied!")) {
         scorePlayer += 1;
         scoreComputer += 1;
-        break;
       }
-      case game.includes("win!"): {
+
+      if (round.includes("win!")) {
         scorePlayer += 1;
-        break;
       }
-      case game.includes("lose!"): {
+
+      if (round.includes("lose!")) {
         scoreComputer += 1;
       }
+      
+      console.log(round);
+      break;
     }
-
-    console.log(game);
-    console.log("Player: " + scorePlayer);
-    console.log("Computer: " + scoreComputer);
   }
+
+  scorePlayer === scoreComputer ? console.log("Game tied!")
+  : scorePlayer < scoreComputer ? console.log("Computer wins!")
+  : console.log("You win!");
+
+  console.log("Player: " + scorePlayer);
+  console.log("Computer: " + scoreComputer);
 }
 
 game();
